05fe34cb71debc0744158ed926ceb491a857e0bb
stackbit-projects <projects@stackbit.com>
2020-08-25 19:51:58 +0000

    Update configuration

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,8 +4,8 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5f456bba3588fa001e224a40/webhook/build/ssgbuild > /dev/null
 
 next build && next export
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5f456bba3588fa001e224a40/webhook/build/publish > /dev/null
